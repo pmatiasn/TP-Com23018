e1dc8cbb83f8f873b83e359d6e51ca4c68fa7106
mposen <pose.matias@gmail.com>
2023-05-09 18:39:59 -0300

    documentacion_js galeria

--- js/galeria.js
+++ js/galeria.js
@@ -1,11 +1,13 @@
-const btnCierra = document.querySelector ('#btn-cierra');
-const btnAdelanta = document.querySelector ('#btn-adelanta');
-const btnRetrocede = document.querySelector ('#btn-retrocede');
-const imagenes = document.querySelectorAll ('#galeria img');
-const contenedorPrincipal = document.querySelector ('#contenedor-principal');
-const imagenActiva = document.querySelector ('#img-activa');
-let indiceImagen = 0;
+/* Variables */
+const btnCierra = document.querySelector ('#btn-cierra'); /*Acceso al boton de cierre*/
+const btnAdelanta = document.querySelector ('#btn-adelanta'); /*Acceso al boton adelanta*/
+const btnRetrocede = document.querySelector ('#btn-retrocede'); /*Acceso al boton retrocede*/
+const imagenes = document.querySelectorAll ('#galeria img'); /*acceso a todas las imagenes del contenedor galeria*/
+const contenedorPrincipal = document.querySelector ('#contenedor-principal'); /*acceso al contenedor principal*/
+const imagenActiva = document.querySelector ('#img-activa'); /*acceso a la imagen activa*/
+let indiceImagen = 0; /*indice imagen/ numero*/
 
+/* Abre el contenedor */
 const abreLightbox = (event) => {
     imagenActiva.src = event.target.src;
     contenedorPrincipal.style.display = 'flex';
@@ -16,10 +18,12 @@ imagenes.forEach((imagen) => {
     imagen.addEventListener ('click', abreLightbox);
 });
 
+/* Cierra el contenedor */
 btnCierra.addEventListener ('click', () => {
     contenedorPrincipal.style.display = 'none';
 });
 
+/* Adelanta imagen */
 const adelantaImagen = () => {
     if (indiceImagen === imagenes.length - 1){
         indiceImagen = -1;
@@ -30,6 +34,7 @@ const adelantaImagen = () => {
 
 btnAdelanta.addEventListener ('click', adelantaImagen);
 
+/* Retrocede imagen */
 const retrocedeImagen = () => {
     if (indiceImagen === 0){
         indiceImagen = imagenes.length;
@@ -39,7 +44,3 @@ const retrocedeImagen = () => {
 };
 
 btnRetrocede.addEventListener ('click', retrocedeImagen);
-
-
-
-
