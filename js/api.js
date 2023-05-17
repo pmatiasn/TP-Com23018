3b4eca168c976aa4f979b1276c330d262e0d7a0e
javier ferrari <javierferrarimezher@gmail.com>
2023-05-16 22:20:55 -0300

    improving code

--- js/api.js
+++ js/api.js
@@ -1,39 +1,44 @@
 const region = document.getElementById('tablaRegion').getAttribute('name');
 console.log(region)
+
+async function apiCall(city) { 
+  try {
+  await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},AR&appid=4ae2636d8dfbdc3044bede63951a019b&units=metric`)
+        .then (resp => resp.json())
+        .then (recurso => {
+          
+          let weatherIcon = "";
+
+          if (recurso.weather[0].main === "Clear" ) {
+            weatherIcon = '<i class="fa-solid fa-sun" style="color: #fbf304;"></i>';
+          } else if (recurso.weather[0].main === "Clouds") {
+            weatherIcon = '<i class="fa-solid fa-cloud" style="color: #0561ff;"></i>'; 
+          } else {
+            weatherIcon = recurso.weather[0].main;
+          }  
+          
+          const newCity = 
+          `<tr>
+          <td>${recurso.name}</td>
+          <td>${recurso.main.temp_max}</td>
+          <td>${recurso.main.temp_min}</td>
+          <td>${weatherIcon}</td>
+          </tr>`   
+
+          document.getElementById('tablaRegion').insertAdjacentHTML('beforeend', newCity);
+        } );
+  
+} catch (error) {
+  console.log(error)
+}
+}
+
 if (region === 'cuyo') {
     
     const cities = ['mendoza', 'san juan', 'san luis', 'la rioja'];
 
     cities.forEach( async city => {
-        try {
-          await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},AR&appid=4ae2636d8dfbdc3044bede63951a019b&units=metric`)
-                .then (resp => resp.json())
-                .then (recurso => {
-                  
-                  let weatherIcon = "";
-
-                  if (recurso.weather[0].main === "Clear" ) {
-                    weatherIcon = '<i class="fa-solid fa-sun" style="color: #fbf304;"></i>';
-                  } else if (recurso.weather[0].main === "Clouds") {
-                    weatherIcon = '<i class="fa-solid fa-cloud" style="color: #0561ff;"></i>'; 
-                  } else {
-                    weatherIcon = recurso.weather[0].main;
-                  }  
-                  
-                  const newCity = 
-                  `<tr>
-                  <td>${recurso.name}</td>
-                  <td>${recurso.main.temp_max}</td>
-                  <td>${recurso.main.temp_min}</td>
-                  <td>${weatherIcon}</td>
-                  </tr>`   
-  
-                  document.getElementById('tablaRegion').insertAdjacentHTML('beforeend', newCity);
-                } );
-          
-        } catch (error) {
-          console.log(error)
-        }
+        apiCall(city)
     })
 }
 
@@ -42,35 +47,7 @@ if (region === 'patagonia') {
     const cities = ['neuquen', 'rio negro', 'rawson', 'santa cruz', 'ushuaia'];
 
     cities.forEach(async city => {
-        try {
-          
-          await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},AR&appid=4ae2636d8dfbdc3044bede63951a019b&units=metric`)
-                .then (resp => resp.json())
-                .then (recurso => {
-
-                  let weatherIcon = "";
-
-                  if (recurso.weather[0].main === "Clear" ) {
-                    weatherIcon = '<i class="fa-solid fa-sun" style="color: #fbf304;"></i>';
-                  } else if (recurso.weather[0].main === "Clouds") {
-                    weatherIcon = '<i class="fa-solid fa-cloud" style="color: #0561ff;"></i>'; 
-                  } else {
-                    weatherIcon = recurso.weather[0].main;
-                  }  
-          
-                  const newCity = 
-                  `<tr>
-                  <td>${recurso.name}</td>
-                  <td>${recurso.main.temp_max}</td>
-                  <td>${recurso.main.temp_min}</td>
-                  <td>${weatherIcon}</td>
-                  </tr>`   
-  
-                  document.getElementById('tablaRegion').insertAdjacentHTML('beforeend', newCity);
-                } );
-        } catch (error) {
-          console.log(error)
-        }
+      apiCall(city)
     })
 }
 
@@ -80,35 +57,7 @@ if (region === 'litoral') {
 
     cities.forEach(async city => {
         
-      try {
-        
-        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},AR&appid=4ae2636d8dfbdc3044bede63951a019b&units=metric`)
-              .then (resp => resp.json())
-              .then (recurso => {
-
-                let weatherIcon = "";
-
-                if (recurso.weather[0].main === "Clear" ) {
-                  weatherIcon = '<i class="fa-solid fa-sun" style="color: #fbf304;"></i>';
-                } else if (recurso.weather[0].main === "Clouds") {
-                  weatherIcon = '<i class="fa-solid fa-cloud" style="color: #0561ff;"></i>'; 
-                } else {
-                  weatherIcon = recurso.weather[0].main;
-                }  
-        
-                const newCity = 
-                `<tr>
-                <td>${recurso.name}</td>
-                <td>${recurso.main.temp_max}</td>
-                <td>${recurso.main.temp_min}</td>
-                <td>${weatherIcon}</td>
-                </tr>`   
-
-                document.getElementById('tablaRegion').insertAdjacentHTML('beforeend', newCity);
-              } );
-            } catch (error) {
-            console.log(error)           
-            }
+      apiCall(city)
           })
 }
 
@@ -118,72 +67,16 @@ if (region === 'pampa') {
 
     cities.forEach( async city => {
         
-      try {
-        
-        await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},AR&appid=4ae2636d8dfbdc3044bede63951a019b&units=metric`)
-               .then (resp => resp.json())
-               .then (recurso => {
-
-                let weatherIcon = "";
-
-                if (recurso.weather[0].main === "Clear" ) {
-                  weatherIcon = '<i class="fa-solid fa-sun" style="color: #fbf304;"></i>';
-                } else if (recurso.weather[0].main === "Clouds") {
-                  weatherIcon = '<i class="fa-solid fa-cloud" style="color: #0561ff;"></i>'; 
-                } else {
-                  weatherIcon = recurso.weather[0].main;
-                }  
-         
-                 const newCity = 
-                 `<tr>
-                 <td>${recurso.name}</td>
-                 <td>${recurso.main.temp_max}</td>
-                 <td>${recurso.main.temp_min}</td>
-                 <td>${weatherIcon}</td>
-                 </tr>`   
- 
-                 document.getElementById('tablaRegion').insertAdjacentHTML('beforeend', newCity);
-               } );
-      } catch (error) {
-        console.log(error)
-      }
+      apiCall(city)
     })
 }
 
 if (region === 'norte') {
     
-    const cities = ['san salvador', 'salta', 'tucuman', 'catamarca', 'santiago del estero', 'la rioja', 'formosa', 'chaco'];
+    const cities = ['san salvador', 'salta', 'tucuman', 'catamarca', 'santiago del estero', 'la rioja', 'formosa', 'resistencia'];
 
     cities.forEach(async city => {
         
-      try {
-        
-        await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},AR&appid=4ae2636d8dfbdc3044bede63951a019b&units=metric`)
-               .then (resp => resp.json())
-               .then (recurso => {
-
-                let weatherIcon = "";
-
-                if (recurso.weather[0].main === "Clear" ) {
-                  weatherIcon = '<i class="fa-solid fa-sun" style="color: #fbf304;"></i>';
-                } else if (recurso.weather[0].main === "Clouds") {
-                  weatherIcon = '<i class="fa-solid fa-cloud" style="color: #0561ff;"></i>'; 
-                } else {
-                  weatherIcon = recurso.weather[0].main;
-                }  
-         
-                 const newCity = 
-                 `<tr>
-                 <td>${recurso.name}</td>
-                 <td>${recurso.main.temp_max}</td>
-                 <td>${recurso.main.temp_min}</td>
-                 <td>${weatherIcon}</td>
-                 </tr>`   
- 
-                 document.getElementById('tablaRegion').insertAdjacentHTML('beforeend', newCity);
-               } );
-      } catch (error) {
-        console.log(error)
-      }
+      apiCall(city)
     })
 }
