a634c6ed94618bbbdb4bf5b94e1ee00698c9e881
javier ferrari <javierferrarimezher@gmail.com>
2023-05-16 22:08:26 -0300

    add awesome icons to weather in api.js

--- js/api.js
+++ js/api.js
@@ -9,13 +9,23 @@ if (region === 'cuyo') {
           await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},AR&appid=4ae2636d8dfbdc3044bede63951a019b&units=metric`)
                 .then (resp => resp.json())
                 .then (recurso => {
-          
+                  
+                  let weatherIcon = "";
+
+                  if (recurso.weather[0].main === "Clear" ) {
+                    weatherIcon = '<i class="fa-solid fa-sun" style="color: #fbf304;"></i>';
+                  } else if (recurso.weather[0].main === "Clouds") {
+                    weatherIcon = '<i class="fa-solid fa-cloud" style="color: #0561ff;"></i>'; 
+                  } else {
+                    weatherIcon = recurso.weather[0].main;
+                  }  
+                  
                   const newCity = 
                   `<tr>
                   <td>${recurso.name}</td>
                   <td>${recurso.main.temp_max}</td>
                   <td>${recurso.main.temp_min}</td>
-                  <td>${recurso.weather[0].main}</td>
+                  <td>${weatherIcon}</td>
                   </tr>`   
   
                   document.getElementById('tablaRegion').insertAdjacentHTML('beforeend', newCity);
@@ -37,13 +47,23 @@ if (region === 'patagonia') {
           await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},AR&appid=4ae2636d8dfbdc3044bede63951a019b&units=metric`)
                 .then (resp => resp.json())
                 .then (recurso => {
+
+                  let weatherIcon = "";
+
+                  if (recurso.weather[0].main === "Clear" ) {
+                    weatherIcon = '<i class="fa-solid fa-sun" style="color: #fbf304;"></i>';
+                  } else if (recurso.weather[0].main === "Clouds") {
+                    weatherIcon = '<i class="fa-solid fa-cloud" style="color: #0561ff;"></i>'; 
+                  } else {
+                    weatherIcon = recurso.weather[0].main;
+                  }  
           
                   const newCity = 
                   `<tr>
                   <td>${recurso.name}</td>
                   <td>${recurso.main.temp_max}</td>
                   <td>${recurso.main.temp_min}</td>
-                  <td>${recurso.weather[0].main}</td>
+                  <td>${weatherIcon}</td>
                   </tr>`   
   
                   document.getElementById('tablaRegion').insertAdjacentHTML('beforeend', newCity);
@@ -65,13 +85,23 @@ if (region === 'litoral') {
         fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},AR&appid=4ae2636d8dfbdc3044bede63951a019b&units=metric`)
               .then (resp => resp.json())
               .then (recurso => {
+
+                let weatherIcon = "";
+
+                if (recurso.weather[0].main === "Clear" ) {
+                  weatherIcon = '<i class="fa-solid fa-sun" style="color: #fbf304;"></i>';
+                } else if (recurso.weather[0].main === "Clouds") {
+                  weatherIcon = '<i class="fa-solid fa-cloud" style="color: #0561ff;"></i>'; 
+                } else {
+                  weatherIcon = recurso.weather[0].main;
+                }  
         
                 const newCity = 
                 `<tr>
                 <td>${recurso.name}</td>
                 <td>${recurso.main.temp_max}</td>
                 <td>${recurso.main.temp_min}</td>
-                <td>${recurso.weather[0].main}</td>
+                <td>${weatherIcon}</td>
                 </tr>`   
 
                 document.getElementById('tablaRegion').insertAdjacentHTML('beforeend', newCity);
@@ -93,13 +123,23 @@ if (region === 'pampa') {
         await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},AR&appid=4ae2636d8dfbdc3044bede63951a019b&units=metric`)
                .then (resp => resp.json())
                .then (recurso => {
+
+                let weatherIcon = "";
+
+                if (recurso.weather[0].main === "Clear" ) {
+                  weatherIcon = '<i class="fa-solid fa-sun" style="color: #fbf304;"></i>';
+                } else if (recurso.weather[0].main === "Clouds") {
+                  weatherIcon = '<i class="fa-solid fa-cloud" style="color: #0561ff;"></i>'; 
+                } else {
+                  weatherIcon = recurso.weather[0].main;
+                }  
          
                  const newCity = 
                  `<tr>
                  <td>${recurso.name}</td>
                  <td>${recurso.main.temp_max}</td>
                  <td>${recurso.main.temp_min}</td>
-                 <td>${recurso.weather[0].main}</td>
+                 <td>${weatherIcon}</td>
                  </tr>`   
  
                  document.getElementById('tablaRegion').insertAdjacentHTML('beforeend', newCity);
@@ -122,14 +162,22 @@ if (region === 'norte') {
                .then (resp => resp.json())
                .then (recurso => {
 
-                console.log(recurso)
+                let weatherIcon = "";
+
+                if (recurso.weather[0].main === "Clear" ) {
+                  weatherIcon = '<i class="fa-solid fa-sun" style="color: #fbf304;"></i>';
+                } else if (recurso.weather[0].main === "Clouds") {
+                  weatherIcon = '<i class="fa-solid fa-cloud" style="color: #0561ff;"></i>'; 
+                } else {
+                  weatherIcon = recurso.weather[0].main;
+                }  
          
                  const newCity = 
                  `<tr>
                  <td>${recurso.name}</td>
                  <td>${recurso.main.temp_max}</td>
                  <td>${recurso.main.temp_min}</td>
-                 <td>${recurso.weather[0].main}</td>
+                 <td>${weatherIcon}</td>
                  </tr>`   
  
                  document.getElementById('tablaRegion').insertAdjacentHTML('beforeend', newCity);
